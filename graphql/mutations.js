177aeaa9bbca2fa6207704c8e8be473838d21d7a
Leon-Arno <leon.rimrod@gmail.com>
2019-04-01 15:27:53 +0200

    Added update resolver

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -23,6 +23,29 @@ const Mutation = new GraphQLObjectType({
 
         return newTech.save();
       }
+    },
+    updateTechnology: {
+      type: techGraphQLType,
+      args: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        release_date: { type: GraphQLString },
+        by_company: { type: GraphQLString },
+        license: { type: GraphQLString }
+      },
+      async resolve(parent, args) {
+        try {
+          const technology = await Tech.findById(args.id);
+          (technology.name = args.name),
+            (technology.release_date = args.release_date),
+            (technology.by_company = args.by_company),
+            (technology.license = args.license);
+          const updatedTech = technology.save();
+          return updatedTech;
+        } catch (error) {
+          throw error;
+        }
+      }
     }
   }
 });
